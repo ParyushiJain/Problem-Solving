aa8abdbec04462d57f440e7939eecad6fcfb20aa
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2022-06-18 03:33:46 +0530

    Time: 21 ms (13.38%), Space: 56.9 MB (5.55%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -0,0 +1,52 @@
+class Solution {
+    List<String> r;
+    public String removeF(String s){
+        String r="";
+        for(int i=1;i<s.length();i++){
+            r+=s.charAt(i);
+            
+        }
+        return r;
+    }
+    public List<String> letterCasePermutation(String s) {
+        String op="";
+        r=new ArrayList();
+        permute(s,op);
+    
+        return r;
+    }
+    public void permute(String s,String op){
+        if(s.length()==0){
+            r.add(op);
+            return;
+        }
+        if((s.charAt(0)>=97 &&s.charAt(0)<=122)){
+         String op1=op+s.charAt(0);
+	        String op2="";
+	        
+	        
+	        op2+=s.charAt(0);
+	        op2=op+op2.toUpperCase();
+	        s=removeF(s);
+	        permute(s,op1);
+	        permute(s,op2);
+        }
+        else if((s.charAt(0)>=65 &&s.charAt(0)<=90)){
+           String op1=op+s.charAt(0);
+	        String op2="";
+	        
+	        
+	        op2+=s.charAt(0);
+	        op2=op+op2.toLowerCase();
+	        s=removeF(s);
+	        permute(s,op1);
+	        permute(s,op2);  
+        }
+        else{
+            String op1="";
+            op1=op+s.charAt(0);
+            s=removeF(s);
+            permute(s,op1);
+        }
+    }
+}
