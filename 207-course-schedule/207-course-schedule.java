00232e060954e52d9e1909d5ce05a40f39470039
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2022-08-08 16:42:55 +0530

    Time: 5 ms (85.83%), Space: 47.1 MB (78.80%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,34 +1,34 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> edges=new ArrayList();
+        ArrayList<ArrayList<Integer>> adj=new ArrayList();
         for(int i=0;i<numCourses;i++){
-            edges.add(new ArrayList());
+            adj.add(new ArrayList());
         }
         for(int i=0;i<prerequisites.length;i++){
-            edges.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
-        int[] vis=new int[numCourses] ;
-        int[] dfsVis=new int[numCourses];
-        for(int i=0;i<numCourses;i++){
+        int s=adj.size();
+        int[] vis=new int[s];
+        int[] dfsVis=new int[s];
+        for(int i=0;i<vis.length;i++){
             if(vis[i]==0){
-                if(dfs(i,vis,dfsVis,edges)) return false;
+                if(dfs(i,adj,vis,dfsVis)) return false;
             }
         }
-       
         return true;
     }
-    public boolean dfs(int i,int[] vis,int[] dfsVis , ArrayList<ArrayList<Integer>> edges){
-        vis[i]=1;
-        dfsVis[i]=1;
-        for(int el:edges.get(i)){
-            if(vis[el]==0){
-               if(dfs(el,vis,dfsVis,edges)) return true;
-            }
-            else if(dfsVis[el]==1){
-                return true;
-            }
-        }
-        dfsVis[i]=0;
-        return false;
-    }
+     public boolean dfs(int i,ArrayList<ArrayList<Integer>> adj,int[] vis,int[] dfsVis){
+         vis[i]=1;
+         dfsVis[i]=1;
+         for(int el:adj.get(i)){
+             if(vis[el]==0){
+                 if(dfs(el,adj,vis,dfsVis)) return true;
+             }
+             else if(vis[el]==dfsVis[el]){
+                 return true;
+             }
+         }
+         dfsVis[i]=0;
+         return false;
+     }
 }
