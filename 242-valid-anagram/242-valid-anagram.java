231b93c8c001577fd260ca01376ca96e0631c093
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2022-08-17 22:13:38 +0530

    Time: 23 ms (20.72%), Space: 46.5 MB (18.48%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,16 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        char[] a=s.toCharArray();
-        Arrays.sort(a);
-        s=new String(a);
-        char[] b=t.toCharArray();
-        Arrays.sort(b);
-        t=new String(b);
+         HashMap<Character,Integer> hm1=new HashMap();
+        
+         HashMap<Character,Integer> hm2=new HashMap();
         for(int i=0;i<s.length();i++){
-           if(s.charAt(i)!=t.charAt(i)) return false; 
-           
+            if(!hm1.containsKey(s.charAt(i))){
+                hm1.put(s.charAt(i),1);
+            }
+            else{
+                hm1.put(s.charAt(i),hm1.get(s.charAt(i))+1);
+            }
+            
         }
-        return true;
+        for(int i=0;i<t.length();i++){
+            if(!hm2.containsKey(t.charAt(i))){
+                hm2.put(t.charAt(i),1);
+            }
+            else{
+                hm2.put(t.charAt(i),hm2.get(t.charAt(i))+1);
+            }
+        }
+        if(hm1.equals(hm2)) return true;
+        else return false;
     }
 }
