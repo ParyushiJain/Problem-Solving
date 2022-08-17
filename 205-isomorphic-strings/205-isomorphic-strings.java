e4b71e37587bdbf73d15eac11ca5808d9f172a03
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2022-08-17 12:32:13 +0530

    Time: 44 ms (8.46%), Space: 54.5 MB (5.11%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,36 +1,23 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,ArrayList<Integer>> hm1=new HashMap();
-        HashMap<Character,ArrayList<Integer>> hm2=new HashMap();
-        for(int i=0;i<s.length();i++){
-            if(!hm1.containsKey(s.charAt(i))){
-                ArrayList<Integer> al=new ArrayList();
-                al.add(i);
-                hm1.put(s.charAt(i),al);
-            }
-            else{
-                ArrayList<Integer> ta=new ArrayList(hm1.get(s.charAt(i)));
-                ta.add(i);
-                hm1.put(s.charAt(i),ta);
-            }
-        }
-           for(int i=0;i<t.length();i++){
-            if(!hm2.containsKey(t.charAt(i))){
-                ArrayList<Integer> al=new ArrayList();
-                al.add(i);
-                hm2.put(t.charAt(i),al);
-            }
-            else{
-                ArrayList<Integer> ta=new ArrayList(hm2.get(t.charAt(i)));
-                ta.add(i);
-                hm2.put(t.charAt(i),ta);
-            }
-        }
-        HashSet<String> set1 = new HashSet(hm1.values());
-  
+    private String transformString(String s) {
+        Map<Character, Integer> indexMapping = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
         
-        HashSet<String> set2 = new HashSet(hm2.values());
-        if(set1.equals(set2)) return true;
-        else return false;
+        for (int i = 0; i < s.length(); ++i) {
+            char c1 = s.charAt(i);
+            
+            if (!indexMapping.containsKey(c1)) {
+                indexMapping.put(c1, i);
+            }
+            
+            builder.append(Integer.toString(indexMapping.get(c1)));
+            builder.append(" ");
+        }
+        return builder.toString();
+    }
+    
+    public boolean isIsomorphic(String s, String t) {
+        System.out.print(transformString(s)+" "+transformString(t));
+        return transformString(s).equals(transformString(t));
     }
 }
