97f517296624ca09d6ec82b7cc2e18507901fd7e
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2022-09-18 12:22:29 +0530

    Time: 1 ms (40.51%), Space: 46.8 MB (78.03%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,36 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-      int[] r=new int[2];
-    
-        int i=bs(0,nums.length-1,nums,target);
-        System.out.print(i);
-        if(i==-1){
-            r[0]=-1;
-            r[1]=-1;
+    public int[] searchRange(int[] n, int t) {
+        int r1=bsf(0,n.length-1,n,t);
+        int r2=bsl(0,n.length-1,n,t);
+        int[] ra=new int[2];
+        ra[0]=r1;
+        ra[1]=r2;
+        return ra;
+    }
+    public int bsf(int s,int e,int[] n,int t){
+        int r=-1;
+        while(s<=e){
+            int m=s+((e-s)/2);
+            if(n[m]==t){
+                r=m;
+                e=m-1;
+            }
+            else if(t>n[m]) s=m+1;
+            else e=m-1;
         }
-        else{
-           int idx=i;
-            int j=i;
-            while(idx>0 && nums[idx-1]==target){
-               idx--; 
-            }
-            r[0]=idx;
-            while(j<nums.length-1 && nums[j+1]==target){
-                j++;
-            }
-           r[1]=j;
-         }
         return r;
     }
-    public int bs(int s,int e,int[] nums,int t){
-        if(s>e){
-            return -1;
+     public int bsl(int s,int e,int[] n,int t){
+        int r=-1;
+        while(s<=e){
+            int m=s+((e-s)/2);
+            if(n[m]==t){
+                r=m;
+                s=m+1;
+            }
+            else if(t>n[m]) s=m+1;
+            else e=m-1;
         }
-        int m=(s+e)/2;
-        if(nums[m]==t){
-            return m;
-        }
-        else if(t<nums[m]){
-            return bs(s,m-1,nums,t);
-        }
-        else{
-            return bs(m+1,e,nums,t);
-        }
+        return r;
     }
 }
