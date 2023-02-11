acf00b8b8c6c7db2acf5b5408a10b0c5a5701fcc
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2023-02-11 19:12:45 +0530

    Added solution - LeetHub

---
+++ Boolean Parenthesization - GFG/boolean-parenthesization.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String S = in.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countWays(N, S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+   static int[][][] dp;
+    static int countWays(int N, String S){
+        // code here
+           dp = new int[N+1][N+1][2];
+      for(int[][] arr2:dp){
+           for(int[] arr1:arr2) 
+                Arrays.fill(arr1,-1);
+      }
+        return solve(S,0,N-1,1);
+    }
+    static int solve(String s,int i,int j,int val){
+        if(i>j) return 0;
+        if(val==1 && dp[i][j][1]!=-1)  return dp[i][j][1];
+        else if(dp[i][j][0]!=-1) return dp[i][j][0];
+        if(i==j){
+             int boolVal=s.charAt(i)=='T'?1:0;
+            if(val==boolVal) return 1;
+            return 0;
+        }
+        int ans=0;
+        for(int k=i+1;k<j;k+=2){
+            int lt=solve(s,i,k-1,1);
+            int lf=solve(s,i,k-1,0);
+            int rt=solve(s,k+1,j,1);
+            int rf=solve(s,k+1,j,0);
+            if(s.charAt(k)=='|'){
+                if(val==1){
+                    ans+=lt*rt+lt*rf+lf*rt;
+                }
+                else{
+                    ans+=lf*rf;
+                }
+            }
+            else if(s.charAt(k)=='&'){
+                if(val==1){
+                    ans+=rt*lt;
+                }
+                else{
+                    ans+=lt*rf+lf*rt+lf*rf;
+                }
+            }
+            else {
+                if(val==1){
+                    ans+=rt*lf+rf*lt;
+                }
+                else{
+                   ans+=rt*lt+rf*lf; 
+                }
+            }
+        }
+      
+        return dp[i][j][val]=ans%1003;
+    }
+}
