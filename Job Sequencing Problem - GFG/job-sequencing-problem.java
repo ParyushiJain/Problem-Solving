225735fb7ce58a7d3b83d783abc65a92d57cfd56
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2023-02-22 15:56:57 +0530

    Added solution - LeetHub

--- Job Sequencing Problem - GFG/job-sequencing-problem.java
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 import java.io.*;
 import java.lang.*;
 import java.util.*;
@@ -38,58 +38,40 @@ class GfG {
             System.out.println (res[0] + " " + res[1]);
         }
     }
-}// } Driver Code Ends
+}
+// } Driver Code Ends
 
 
 class Solution
 {
     //Function to find the maximum profit and the number of jobs done.
-    class itemComparator implements Comparator<Job>
-{
-      public int compare(Job a, Job b)
-    {
-        int r1 = a.profit;
-        int r2 = b.profit;
-        if(r1 > r2){
-            return -1;
-        } else if(r1 < r2) {
-            return 1;
-        }
-        return 0;
-    }
-}
-public static int max(Job arr[]){
-    int max=arr[0].deadline;
-    for(int i=1;i<arr.length;i++){
-        if(arr[i].deadline>max){
-            max=arr[i].deadline;
-        }
-    }
-    return max;
-}
     int[] JobScheduling(Job arr[], int n)
     {
-        int r[]=new int[2];
-        r[0]=0;
-        r[1]=0;
-      Arrays.sort(arr,new itemComparator()); 
-      int maxd=max(arr);
-      int[] slot=new int[maxd];
-     
-      for(int i=0;i<arr.length;i++){
-          int t=arr[i].deadline-1;
-          while(t>=0){
-              if(slot[t]!=1){
-                  slot[t]=1;
-                  r[0]++;
-                  r[1]+=arr[i].profit;
-                  break;
-              }
-              t--;
-          }
-          
-      }
-      return r;
+        // Your code here
+       Arrays.sort(arr,(a,b)->b.profit-a.profit);
+        int jd=0;
+        int pr=0;
+        int mx=0;
+        for(Job i:arr){
+            mx=Math.max(mx,i.deadline);
+        }
+        int[] dl=new int[mx+1];
+        Arrays.fill(dl,-1);
+        for(int i=0;i<n;i++){
+            int d=arr[i].deadline;
+            for(int j=d;j>0;j--){
+            if(dl[j]==-1){
+                dl[j]=i;
+                jd++;
+                pr+=arr[i].profit;
+                break;
+            }
+            }
+        }
+        int[] r=new int[2];
+        r[0]=jd;
+        r[1]=pr;
+        return r;
     }
 }
 
