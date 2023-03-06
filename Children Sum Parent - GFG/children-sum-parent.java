b851989fe2adec5f4a5100293246da9757e4685b
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2023-03-06 19:03:36 +0530

    Added solution - LeetHub

--- Children Sum Parent - GFG/children-sum-parent.java
+++ Children Sum Parent - GFG/children-sum-parent.java
@@ -129,32 +129,36 @@ class Node{
 }
 
 */
-
 class Tree
 {
     //Function to check whether all nodes of a tree have the value 
     //equal to the sum of their child nodes.
-    
     public static int isSumProperty(Node root)
     {
         // add your code here
-       if(root.left!=null && root.right!=null) {
-           if(root.left.data + root.right.data == root.data) {
-              return isSumProperty(root.left) & isSumProperty(root.right);
-           }
-           else return 0;
-       }
-       if(root.left==null && root.right!=null){
-           if( root.data==root.right.data) return isSumProperty(root.right);
-           else return 0;
-       } 
-       if(root.left!=null && root.right==null ){
-           if(root.data==root.left.data) return isSumProperty(root.left);
-           else return 0;
-       }
-       
-       return 1;
+        if(root.left!=null && root.right!=null){
+            if(root.left.data+root.right.data==root.data){
+                return isSumProperty(root.left) & isSumProperty(root.right);
+               
+            }
+             else return 0;
+            
+        }
+        if(root.left==null && root.right!=null){
+            if(root.right.data==root.data){
+                return isSumProperty(root.right);
+            }
+            else return 0;
+            
+        }
+        if(root.right==null && root.left!=null){
+            if(root.left.data==root.data){
+                return isSumProperty(root.left);
+            }
+            else return 0;
+        }
+        return 1;
+        
     }
-   
-    
+
 }
