0f812fe9670cab6cd9155e1dba5cb4e8813d4d74
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2023-07-17 16:38:10 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -49,28 +49,21 @@ class gfg
 class Solution 
 { 
     //Function to return max value that can be put in knapsack of capacity W.
-    
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
-         
-        int[][] t=new int[n+1][W+1];
-        return ks(W,wt,val,n,t);
+         // your code here 
+         int[][] dp=new int[n+1][W+1];
+         for(int i=1;i<=n;i++){
+             for(int j=1;j<=W;j++){
+                 if(wt[i-1]<=j){
+                     dp[i][j]=Math.max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                 }
+                 else dp[i][j]=dp[i-1][j];
+             }
+         }
+         return dp[n][W];
+        
     } 
-    static int ks(int W, int wt[], int val[], int n,int[][] t){
-         
-         if(n==0 || W==0){
-             return 0;
-         }
-         if(t[n][W]!=0) return t[n][W];
-         
-         if(wt[n-1]<=W){
-             return t[n][W]=Math.max((val[n-1]+ks(W-wt[n-1],wt,val,n-1,t)),ks(W,wt,val,n-1,t));
-         }
-         else if(wt[n-1]>W) {
-             return t[n][W]=ks(W,wt,val,n-1,t);
-         }
-         return 0;
-    }
 }
 
 
