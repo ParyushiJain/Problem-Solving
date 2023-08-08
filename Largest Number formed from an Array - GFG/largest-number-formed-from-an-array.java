bc260b9413913ae15ebe8afeaa8d0bd07da54496
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2023-08-08 15:59:32 +0530

    Added solution - LeetHub

---
+++ Largest Number formed from an Array - GFG/largest-number-formed-from-an-array.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] arr = br.readLine().trim().split(" ");
+
+            String ans = new Solution().printLargest(arr);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String printLargest(String[] arr) {
+        // code here
+        Arrays.sort(arr, (b,a)->(a+b).compareTo(b+a));
+        String s = "";
+        for(String t : arr) s+=t;
+        return s;
+        }
+}
