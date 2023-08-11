9441e8c4180cd7a4d49b0e00628b9569be9ebc4c
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2023-08-11 17:25:41 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void Rearrange(int a[], int n)
+    {
+        // Your code goes here
+        int[] arr=new int[n];
+        int idx=0;
+        for(int i:a){
+            if(i<0) arr[idx++]=i;
+        }
+        for(int i:a){
+            if(i>=0) arr[idx++]=i;
+        }
+        idx=0;
+        for(int i=0;i<n;i++){
+            a[i]=arr[i];
+        }
+    }
+}
